c6db33dac525877478e954a4bed16b0a1a122c0a
Sanya Gupta <88279411+sanyagup@users.noreply.github.com>
2025-06-10 14:14:38 -0400

    finished the first part of the website

--- sanyagupta-website/src/index.js
+++ sanyagupta-website/src/index.js
@@ -4,6 +4,7 @@ import 'w3-css/w3.css';
 import {useRef} from 'react';
 // import Button from 'react-bootstrap/Button';
 import myPhoto from './IMG_0306.jpg'
+import portofolioTitle from './Screenshot_2025-06-10_105058-removebg-preview.png'
 import './index.css';
 
 const Home = () => {
@@ -27,20 +28,22 @@ const Home = () => {
         </header>
 
         <section className="hero">
-          <div className='left-panel'>
-            <img src={myPhoto} alt="Sanya Gupta" className="hero-img"></img>
-            <p>I love data science and cybersecurity. Blah blah blah</p>
-          </div>
+          <div className='center'>
+            <div className='left-panel w3-animate-top'>
+              <img src={myPhoto} alt="Sanya Gupta" className="hero-img"></img>
+              <p>I love data science and cybersecurity. Blah blah blah</p>
+            </div>
 
-          <div className="right-panel">
-            <h2 className="portfolio-title">PORTFOLIO</h2>
-            <div className="portfolio-outline">PORTFOLIO</div>
-            <div className='social-links'>
-              <p><strong>LI & EM: </strong>sanyagup07</p>
+            <div className="right-panel w3-animate-top">
+              <img src={portofolioTitle} alt="portfolioTitle" className="portfolio-title"></img>
+              <div className='social-links'>
+                <p><strong>LI & EM: </strong>sanyagup07</p>
+              </div>
             </div>
           </div>
+          
         </section>
-        <div className="scroll-indicator" onClick={scrollToAbout}>Scroll down</div>
+        <div className="scroll-indicator w3-animate-top" onClick={scrollToAbout}>Scroll down</div>
       </div>
     </>
   );
